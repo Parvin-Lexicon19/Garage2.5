5f529f1e8fd2389751199cd7bae80b5bd49f63b6
Radhika Jayaraman <radhika_jram@outlook.com>
2020-01-02 16:32:01 +0100

    Added Error message for Member Id and Vehicle Type

--- Garage2.5/ViewModels/VehicleListDetails.cs
+++ Garage2.5/ViewModels/VehicleListDetails.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Garage2._5.ViewModel
 {
@@ -11,9 +12,10 @@ namespace Garage2._5.ViewModel
         public string RegNo { get; set; }
         public string Type { get; set; }
 
+        [Display(Name = "CheckIn Time")]
         public DateTime CheckInTime { get; set; }
 
-
+        [Display(Name = "Owner Name")]
         public string OwnerName { get; set; }
 
 
