8ea74e64ad00635e59a1e3469bdd8c3fc1be0ffe
Radhika Jayaraman <radhika_jram@outlook.com>
2020-01-02 14:35:11 +0100

    Changed the Mapper files

--- Garage2.5/Data/MapperProfile.cs
+++ Garage2.5/Data/MapperProfile.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Garage2._5.ViewModel;
 
 namespace Garage2._5.Data
 {
@@ -17,42 +18,18 @@ namespace Garage2._5.Data
                         dest => dest.ParkedVehiclesNo,
                         from => from.MapFrom(m => m.ParkedVehicles.Count));
 
-                //.ForMember(
-                //       dest => dest.ParkedVehicles,
-                //       from => from.MapFrom(m => m.ParkedVehicles.ToList()));
-        }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
-using Garage2._5.Models;
-using Garage2._5.ViewModel;
-
-namespace Garage2._5.Data
-{
-    public class MapperProfile : Profile
-    {
-        public MapperProfile()
-        {
+            //.ForMember(
+            //       dest => dest.ParkedVehicles,
+            //       from => from.MapFrom(m => m.ParkedVehicles.ToList()));
             CreateMap<ParkedVehicle, VehicleListDetails>()
-               .ForMember(
-                       dest => dest.Type,
-                       from => from.MapFrom(s => s.VehicleType.Type))
-               .ForMember(
-                       dest => dest.OwnerName,
-                       from => from.MapFrom(s => s.Member.FullName));
-
-
-              /* .ForMember(
-                      dest => dest.FirstName,
-                      from => from.MapFrom(s => s.Member.FirstName))
-               .ForMember(
-                      dest => dest.LastName,
-                      from => from.MapFrom(s => s.Member.LastName)); */
-
+          .ForMember(
+                  dest => dest.Type,
+                  from => from.MapFrom(s => s.VehicleType.Type))
+          .ForMember(
+                  dest => dest.OwnerName,
+                  from => from.MapFrom(s => s.Member.FullName));
         }
     }
 }
+
+
