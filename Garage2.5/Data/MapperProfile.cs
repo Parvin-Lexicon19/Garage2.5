60d853185cd8b4646fc31aa33ce700c200a50688
parvin.shafiee.m@gmail.com <parvin.shafiee.m@gmail.com>
2020-01-02 15:11:38 +0100

    OnlyParkedVehiclesAreShownInMemberDetails

--- Garage2.5/Data/MapperProfile.cs
+++ Garage2.5/Data/MapperProfile.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Garage2._5.ViewModel;
 
 namespace Garage2._5.Data
 {
@@ -13,46 +14,28 @@ namespace Garage2._5.Data
         public MapperProfile()
         {
             CreateMap<Member, MemberDetailsViewModel>()
-                .ForMember(
-                        dest => dest.ParkedVehiclesNo,
-                        from => from.MapFrom(m => m.ParkedVehicles.Count));
-
                 //.ForMember(
-                //       dest => dest.ParkedVehicles,
-                //       from => from.MapFrom(m => m.ParkedVehicles.ToList()));
-        }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
-using Garage2._5.Models;
-using Garage2._5.ViewModel;
+                //        dest => dest.ParkedVehiclesNo,
+                //        from => from.MapFrom(m => m.ParkedVehicles.Count))
+            .ForMember(
+                   dest => dest.ParkedVehicles,
+                   from => from.MapFrom(m => m.ParkedVehicles.Where(p => (p.CheckOutTime) == default(DateTime)).ToList()));
 
-namespace Garage2._5.Data
-{
-    public class MapperProfile : Profile
-    {
-        public MapperProfile()
-        {
             CreateMap<ParkedVehicle, VehicleListDetails>()
-               .ForMember(
-                       dest => dest.Type,
-                       from => from.MapFrom(s => s.VehicleType.Type))
-               .ForMember(
-                       dest => dest.OwnerName,
-                       from => from.MapFrom(s => s.Member.FullName));
+           .ForMember(
+                   dest => dest.Type,
+                   from => from.MapFrom(s => s.VehicleType.Type))
+           .ForMember(
+                   dest => dest.OwnerName,
+                   from => from.MapFrom(s => s.Member.FullName));
 
 
-              /* .ForMember(
-                      dest => dest.FirstName,
-                      from => from.MapFrom(s => s.Member.FirstName))
-               .ForMember(
-                      dest => dest.LastName,
-                      from => from.MapFrom(s => s.Member.LastName)); */
-
+            /* .ForMember(
+                    dest => dest.FirstName,
+                    from => from.MapFrom(s => s.Member.FirstName))
+             .ForMember(
+                    dest => dest.LastName,
+                    from => from.MapFrom(s => s.Member.LastName)); */
         }
     }
 }
